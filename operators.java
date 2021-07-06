265da3d945bbedfaa7e9119c11519eefa14829d8
Kartheek Molaganuru <61461978+kartheek562@users.noreply.github.com>
2021-07-06 15:02:16 +0530

    Create operators.java
    it contain all the four arithmetic in a single program

---
+++ operators.java
@@ -0,0 +1,27 @@
+package LEARNING.ADD.OPERATOR;
+
+import java.util.Scanner;
+
+public class ADD {
+
+	public static void main(String[] args) {
+		int a,b,c,d,e,f;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the first number :");
+		a = sc.nextInt();
+		System.out.println("enter the second number : ");
+		b = sc.nextInt();
+		c = a+b;
+		d=a-b;
+		e=a*b;
+		f=a/b;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
