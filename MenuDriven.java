368b1f21d9e00f9c281653a5683738294d114dff
Kartheek Molaganuru <61461978+kartheek562@users.noreply.github.com>
2021-07-06 15:26:53 +0530

    Create MenuDriven.java

---
+++ MenuDriven.java
@@ -0,0 +1,31 @@
+//program to menu driven using switch case
+
+package LEARNING.ADD.OPERATOR;
+
+import java.util.Scanner;
+
+public class maximum {
+
+	public static void main(String[] args) {
+	int a,b,c;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the a value:");
+    a =sc.nextInt();
+    System.out.println("enter the b value:");
+    b =sc.nextInt();
+    System.out.println("enter the c value:");
+    c =sc.nextInt();
+    if (a>b && a>c) {
+    	 System.out.println("the maximum  value is:"+a);
+    }
+    else if(b>a && b>c){
+    	 System.out.println("the maximum  value is:"+b);
+    	
+    }
+    else  {
+    	 System.out.println(" the maximum value is:"+c);
+    }
+    
+	}
+
+}
