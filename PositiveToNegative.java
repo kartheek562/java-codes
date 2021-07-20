47e803a0d7aad5a146c3346e77b41df294be0ccd
Kartheek Molaganuru <61461978+kartheek562@users.noreply.github.com>
2021-07-20 09:24:21 +0530

    Create PositiveToNegative.java

---
+++ PositiveToNegative.java
@@ -0,0 +1,22 @@
+package LEARNING.ADD.OPERATOR;
+import java.util.*;
+
+public class PositiveOrNegative {
+	public static void main(String[]args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the number:");
+		n=sc.nextInt();
+		
+			if(n>=0) {
+				System.out.println("positive number");
+			}
+			else {
+				System.out.println("negative number");
+			}
+		}
+	
+		
+	}
+
